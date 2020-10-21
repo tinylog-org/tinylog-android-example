cf625ad2c9a152d5cf60a30ebe419148d437eb76
Martin Winandy <martin.winandy@pmwmedia.de>
2020-10-22 00:31:27 +0200

    Add rolling file writer

--- app/src/main/java/com/example/tinylog/demo/MainActivity.java
+++ app/src/main/java/com/example/tinylog/demo/MainActivity.java
@@ -5,11 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import org.tinylog.Logger;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        File directoryForLogs = getExternalFilesDir(null);
+        System.setProperty("tinylog.directory", directoryForLogs.getAbsolutePath());
+
         Logger.info("Hello World!");
+        Logger.info("Logs can be found in \"{}\"", directoryForLogs);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
